422da27c86cf6dae34e974f2aa34aa6ffb0a6a36
AFarforov <anton@black-sheep.ru>
2012-06-07 22:46:58 +0400

    v2.0-89

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,21 @@ module ApplicationHelper
     end
   end
 
+  def controls_panel(object)
+    edit_path = send("edit_#{object.class.to_s.downcase}_path", object)
+
+    begin
+      confirm_text = "Do you realy want delete \"#{object.title}\"?"
+    rescue NoMethodError
+      confirm_text = 'Are you sure?'
+    end
+
+    links = nil
+    links  = "<li>" + link_to('Edit', edit_path) + "</li>" if can?(:update, object)
+    links += "<li>" + link_to('Delete', object, :confirm => confirm_text, :method => :delete)  + "</li>" if can?(:destroy, object)
+    links = '<div class="panel-admin"><ul>' + links + '</ul></div><div class="clearfix"></div>' if can?(:manage, object)
+
+    raw links
+  end
+
 end
